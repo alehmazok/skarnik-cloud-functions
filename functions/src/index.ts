8a4ecfbd4e4fd7208b1522b8e05b67a3dc43df09
Aleh Mazok <mazokoleg@gmail.com>
2023-09-27 11:36:30 +0200

    Рэфактарынг.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,30 +9,66 @@
 
 import {analytics} from "firebase-functions/v1";
 import * as logger from "firebase-functions/logger";
-import {AnalyticsEvent} from "firebase-functions/lib/v1/providers/analytics";
-import {sendTelegram} from "./telegram";
+import {AnalyticsEvent, UserDimensions} from "firebase-functions/lib/v1/providers/analytics";
 import * as admin from "firebase-admin";
+import {sendTelegramString} from "./telegram";
 
 admin.initializeApp();
 
-export const logTranslation = analytics.event("translation").onLog((event) => {
-    logger.info("Event", event.params);
-    const word = event.params.word;
-    const direction = event.params.dict_name;
-    const uri = event.params.uri;
-    const message = `
+export const logTranslation = analytics
+    .event("translation")
+    .onLog((event) => {
+        logger.info("Event", event.params);
+        const word = event.params.word;
+        const direction = event.params.dict_name;
+        const uri = event.params.uri;
+        const message = `
 word: <code>${word}</code>
 direction: <code>${direction}</code>
-link: <a href="${uri}"><code>${uri}</code></a>`;
-    return processAnalyticsEvent(event, message);
-});
+link: <a href="${uri}">${uri}</a>`;
+        return processAnalyticsEvent(event, message);
+    });
 
-async function processAnalyticsEvent(event: AnalyticsEvent, message = ""): Promise<void> {
-    const date = new Date(event.logTime).toLocaleString(
+async function processAnalyticsEvent(event: AnalyticsEvent, content = ""): Promise<void> {
+    const tag = event.name;
+    const time = composeTime(event.logTime);
+    const userInfo = composeUserInfo(event.user);
+    const message = composeMessage(tag, time, userInfo, content);
+    return sendTelegramString(message);
+}
+
+function composeTime(logTime: string) {
+    return new Date(logTime).toLocaleString(
         "be-BY",
         {
             timeZone: "Europe/Warsaw",
         },
     );
-    return sendTelegram(`#${event.name}\n${message}\nTime: <code>${date}</code>`, event.user);
 }
+
+function composeUserInfo(user?: UserDimensions): string {
+    // const uid = user?.userId;
+    const language = user?.deviceInfo?.userDefaultLanguage;
+    const brandName = user?.deviceInfo?.mobileBrandName;
+    const marketingName = user?.deviceInfo?.mobileMarketingName;
+    const platformVersion = user?.deviceInfo?.platformVersion;
+    const appPlatform = user?.appInfo?.appPlatform;
+    const country = user?.geoInfo?.country;
+    const city = user?.geoInfo?.city;
+    const appVersion = user?.appInfo?.appVersion;
+    const appStore = user?.appInfo?.appStore;
+
+    return `language: <code>${language}</code>
+location: <code>${city}</code>, <code>${country}</code>
+device: <code>${brandName}</code>, <code>${marketingName}</code>, <code>${appPlatform}</code> <code>${platformVersion}</code>
+version: <code>${appVersion}</code>
+source: <code>${appStore}</code>`;
+}
+
+function composeMessage(tag: string, time: string, userInfo: string, content: string) {
+    return `#${tag}
+<code>${time}</code>
+${userInfo}
+${content}`;
+}
+
--- functions/src/telegram.ts
+++ functions/src/telegram.ts
@@ -1,61 +1,10 @@
 import * as functions from "firebase-functions";
-import {UserDimensions} from "firebase-functions/lib/v1/providers/analytics";
 import * as logger from "firebase-functions/logger";
 import axios, {isAxiosError} from "axios";
 
 
 const apiToken = functions.config().skarnik.telegram.dev_group.api_token;
 const chatId = functions.config().skarnik.telegram.dev_group.chat_id;
-// const emoji = {
-//     "aut": "🇦🇹",
-//     "blr": "🇧🇾",
-//     "can": "🇨🇦",
-//     "che": "🇨🇭",
-//     "cze": "🇨🇿",
-//     "deu": "🇩🇪",
-//     "dnk": "🇩🇰",
-//     "est": "🇪🇪",
-//     "fin": "🇫🇮",
-//     "fra": "🇫🇷",
-//     "gbr": "🇬🇧",
-//     "hun": "🇭🇺",
-//     "kaz": "🇰🇿",
-//     "ltu": "🇱🇹",
-//     "lva": "🇱🇻",
-//     "nor": "🇳🇴",
-//     "pol": "🇵🇱",
-//     "rus": "🇷🇺",
-//     "svk": "🇸🇰",
-//     "svn": "🇸🇮",
-//     "swe": "🇸🇪",
-//     "ukr": "🇺🇦",
-//     "usa": "🇺🇸",
-// };
-
-
-function userInfoString(user?: UserDimensions): string {
-    // const uid = user?.userId;
-    const language = user?.deviceInfo?.userDefaultLanguage;
-    const brandName = user?.deviceInfo?.mobileBrandName;
-    const marketingName = user?.deviceInfo?.mobileMarketingName;
-    const platformVersion = user?.deviceInfo?.platformVersion;
-    const appPlatform = user?.appInfo?.appPlatform;
-    const country = user?.geoInfo?.country;
-    const city = user?.geoInfo?.city;
-    const appVersion = user?.appInfo?.appVersion;
-    const appStore = user?.appInfo?.appStore;
-
-    return `
-language: <code>${language}</code>
-location: <code>${city}</code>, <code>${country}</code>
-device: <code>${brandName}</code>, <code>${marketingName}</code>, <code>${appPlatform}</code>, <code>${platformVersion}</code>
-version: <code>${appVersion}</code>
-source: <code>${appStore}</code>`;
-}
-
-export async function sendTelegram(message: string, user?: UserDimensions): Promise<void> {
-    return sendTelegramString(message + userInfoString(user));
-}
 
 export async function sendTelegramString(message: string): Promise<void> {
     logger.info(`Sending message to Telegram, token=${apiToken}, chatId=${chatId}`);
